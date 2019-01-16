52a5e892f2eb8d87b86aa781fa199d9224ca15e6
Anthony Graniero <ananfro@gmail.com>
2019-01-16 21:10:51 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    self.artist.count 
+    self.artists.count 
   end
 
   def all_artist_names
-    self.artists.collect {|t| t.names
+    self.artists.collect {|t| t.name}
   end
 end
